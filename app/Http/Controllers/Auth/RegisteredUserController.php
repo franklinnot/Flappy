644d6fed55d52db46fd5334be7515c35eb984617
franklinnot <franklinnot@outlook.com>
2025-05-06 02:37:14 -0500

    feat(auth): add role selection to user registration form
    Enhance the registration process by allowing users to select their role during sign-up. This includes updating the backend validation and storage logic, as well as adding a role dropdown to the frontend form. The changes ensure that the role is properly validated and stored in the database, improving the overall user management system.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\Status;
+use App\Enums\UserRol;
+use App\Helpers\EnumHelper;
+use App\Helpers\Report;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
+    public const COMPONENT = "Auth/Register";
+
     /**
      * Display the registration view.
      */
@@ -29,24 +32,27 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request): Response
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'dni' => 'required|digits:8|unique:'.User::class,
+            'dni' => 'required|digits:8|unique:users,dni',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'rol' => ['required', Rule::in(UserRol::names())],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'dni' => $request->dni,
             'password' => Hash::make($request->password),
+            'rol' => EnumHelper::getValue(UserRol::class, $request->rol),// Convierte el nombre del caso al valor respaldado
+            'status' => Status::ACTIVE->value,
         ]);
 
-        event(new Registered($user));
+        if (!$user) {
+            Report::error('Error al registrar un nuevo usuario');
+        }
 
-        Auth::login($user);
-
-        return redirect(RouteServiceProvider::HOME);
+        return Inertia::render(self::COMPONENT, Report::success('Usuario registrado correctamente'));
     }
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,29 +1,30 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, useForm } from "@inertiajs/react";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        dni: '',
-        password: '',
-        password_confirmation: '',
+        name: "",
+        dni: "",
+        password: "",
+        password_confirmation: "",
+        rol: "",
     });
 
     useEffect(() => {
         return () => {
-            reset('password', 'password_confirmation');
+            reset("password", "password_confirmation");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('register'));
+        post(route("register"));
     };
 
     return (
@@ -32,7 +33,7 @@ export default function Register() {
 
             <form onSubmit={submit}>
                 <div>
-                    <InputLabel htmlFor="name" value="Name" />
+                    <InputLabel htmlFor="name" value="Nombre" />
 
                     <TextInput
                         id="name"
@@ -41,7 +42,7 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="name"
                         isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
+                        onChange={(e) => setData("name", e.target.value)}
                         required
                     />
 
@@ -58,7 +59,7 @@ export default function Register() {
                         value={data.dni}
                         className="mt-1 block w-full"
                         autoComplete="username"
-                        onChange={(e) => setData('dni', e.target.value)}
+                        onChange={(e) => setData("dni", e.target.value)}
                         required
                     />
 
@@ -66,7 +67,7 @@ export default function Register() {
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                    <InputLabel htmlFor="password" value="Contraseña" />
 
                     <TextInput
                         id="password"
@@ -75,7 +76,7 @@ export default function Register() {
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                         required
                     />
 
@@ -83,7 +84,10 @@ export default function Register() {
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+                    <InputLabel
+                        htmlFor="password_confirmation"
+                        value="Confirma la contraseña"
+                    />
 
                     <TextInput
                         id="password_confirmation"
@@ -92,23 +96,46 @@ export default function Register() {
                         value={data.password_confirmation}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
+                        onChange={(e) =>
+                            setData("password_confirmation", e.target.value)
+                        }
                         required
                     />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                    <InputError
+                        message={errors.password_confirmation}
+                        className="mt-2"
+                    />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="roles" value="Rol" />
+
+                    <select
+                        id="roles"
+                        name="roles"
+                        value={data.rol} 
+                        className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-1"
+                        onChange={(e) => setData("rol", e.target.value)}
+                        required
+                    >
+                        <option className="text-sm" value="">
+                            Selecciona una opción...
+                        </option>
+                        <option className="text-sm" value="ADMIN">
+                            Administrador
+                        </option>
+                        <option className="text-sm" value="SELLER">
+                            Vendedor
+                        </option>
+                    </select>
+
+                    <InputError message={errors.rol} className="mt-2" />
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
+                    <PrimaryButton className="" disabled={processing}>
+                        Registrar
                     </PrimaryButton>
                 </div>
             </form>
