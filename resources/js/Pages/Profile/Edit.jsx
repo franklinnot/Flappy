cb1e3763943955de3e3c7012fd73058833bf330e
franklinnot <franklinnot@outlook.com>
2025-05-06 02:36:47 -0500

    refactor: remove unused props and components from profile and auth views
    Clean up the code by removing unused props (`mustVerifyEmail`, `canResetPassword`) and components (`DeleteUserForm`) from the profile and authentication views. This improves maintainability and reduces unnecessary complexity in the codebase.

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,10 +1,9 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 import { Head } from '@inertiajs/react';
 
-export default function Edit({ auth, mustVerifyEmail, status }) {
+export default function Edit({ auth, status }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -16,7 +15,6 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                     <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                         <UpdateProfileInformationForm
-                            mustVerifyEmail={mustVerifyEmail}
                             status={status}
                             className="max-w-xl"
                         />
@@ -24,10 +22,6 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
 
                     <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                         <UpdatePasswordForm className="max-w-xl" />
-                    </div>
-
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <DeleteUserForm className="max-w-xl" />
                     </div>
                 </div>
             </div>
