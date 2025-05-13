c1e379ec4dc7892c6db26025a20c6b485ff10a24
franklinnot <franklinnot@outlook.com>
2025-05-12 22:44:12 -0500

    fix(Authenticate): ensure user is authenticated and enabled
    The middleware now checks if the user is both authenticated and has a status of 'Habilitado' before allowing access. This prevents unauthorized or disabled users from accessing protected routes.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,19 +6,17 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use App\Enums\Status;
 
 class Authenticate
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * Verify that the user is authenticated and their status is 'Habilitado'
      */
     public function handle(Request $request, Closure $next)
     {
         $user = Auth::user();
-
-        // Verificar si el usuario está autenticado
-        if (!$user) {
+        if (!$user || $user->status != Status::ENABLED->value) {
             return Inertia::location(route('login'));
         } else {
             return $next($request);
