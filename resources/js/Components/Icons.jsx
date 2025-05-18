e270978ca4d603ba515f01034dbec21ce9549054
franklinnot <franklinnot@outlook.com>
2025-05-18 17:25:24 -0500

    feat(Icons): add new icons for check, warning, error, info, and exit
    This commit introduces several new icon components (IconCheckCircle, IconCheck, IconWarning, IconError, IconInformation, IconExit) to enhance the UI with additional visual indicators. These icons will be used across the application for better user feedback and navigation.

--- resources/js/Components/Icons.jsx
+++ resources/js/Components/Icons.jsx
@@ -76,7 +76,6 @@ export const MenuBurger = (props) => (
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
         />
     </Icon>
-    
 );
 
 export const IconProfile = (props) => (
@@ -89,3 +88,63 @@ export const IconProfile = (props) => (
     </Icon>
 );
 
+export const IconCheckCircle = (props) => (
+    <Icon {...props} fill="currentColor" stroke="none">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+    </Icon>
+);
+
+export const IconCheck = (props) => (
+    <Icon {...props} fill="currentColor" stroke="none">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 12.75 6 6 9-13.5"
+        />
+    </Icon>
+);
+
+// triangle
+export const IconWarning = (props) => (
+    <Icon {...props} fill="currentColor" stroke="none">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+        />
+    </Icon>
+);
+
+export const IconError = (props) => (
+    <Icon {...props} fill="currentColor" stroke="none">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+        />
+    </Icon>
+);
+
+export const IconInformation = (props) => (
+    <Icon {...props} fill="currentColor" stroke="none">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+        />
+    </Icon>
+);
+
+export const IconExit = (props) => (
+    <Icon {...props} fill="currentColor" stroke="none">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+        />
+    </Icon>
+);
