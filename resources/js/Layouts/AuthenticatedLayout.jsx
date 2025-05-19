139f50ac50d94206fb4663949fc8207f34dc380f
franklinnot <franklinnot@outlook.com>
2025-05-18 19:20:19 -0500

    style(Layouts): improve responsive design and clean up layout components
    - Hide user name and logo on small screens in ProfileMenu and Header
    - Remove redundant placeholder class and simplify grid structure in AuthenticatedLayout
    - Adjust header height and border styling for consistency

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,14 +1,12 @@
 import Header from "@/Layouts/Partials/Header";
 import { usePage } from "@inertiajs/react";
 
-export default function Authenticated({ children, title}) {
+export default function Authenticated({ children, title = null }) {
     const user = usePage().props?.auth?.user;
     return (
-        <div className="min-h-dvh placeholder:grid grid-rows-[auto_1fr] bg-white">
+        <div className="min-h-dvh grid grid-rows-[auto_1fr] bg-white">
             <Header user={user} title={title}></Header>
-            <main className="p-4 pb-0">
-                {children}
-            </main>
+            <main className="p-4 pb-0">{children}</main>
         </div>
     );
 }
--- resources/js/Layouts/Partials/Header.jsx
+++ resources/js/Layouts/Partials/Header.jsx
@@ -6,16 +6,24 @@ import NavigationMenu from "@/Layouts/Partials/NavigationMenu";
 
 export default function Header({ user, title = null }) {
     return (
-        <header className="sticky top-0 z-50 grid grid-flow-col place-items-center px-5 lg:px-6 h-14 sm:max-h-16 bg-[#EFF4FF] border-b-2">
+        <header className="sticky top-0 z-50 grid grid-flow-col place-items-center px-5 lg:px-6 h-[3.3rem] sm:max-h-16 bg-[#EFF4FF] border-b border-gray-300">
             <div className="grid grid-flow-col place-items-center justify-self-start gap-3">
                 <NavigationMenu user={user} />
                 {/* Application logo */}
-                <Link className="inline-flex ml-1" href={route("profile")}>
+                <Link
+                    className="hidden sm:inline-flex ml-1"
+                    href={route("profile")}
+                >
                     <ApplicationLogo className="size-[38px]" />
                 </Link>
-                {/* Page title */}
-                <h1 className="text-lg font-medium text-gray-600 mt-1">{title}</h1>
             </div>
+
+            {/* Page title */}
+            {title && (
+                <h1 className="text-lg font-medium text-gray-700 mt-1 flex relative items-center justify-center">
+                    <span className="absolute text-nowrap">{title}</span>
+                </h1>
+            )}
 
             {/* profile button */}
             <div className="grid grid-flow-col place-items-center justify-self-end gap-2 mt-1">
