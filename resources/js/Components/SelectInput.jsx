2e37ee6784c000936387064afca837f3886a8bc8
franklinnot <franklinnot@outlook.com>
2025-06-09 23:24:43 -0500

    feat: enhance SelectInput component with click outside functionality and refactor code

--- resources/js/Components/SelectInput.jsx
+++ resources/js/Components/SelectInput.jsx
@@ -1,12 +1,6 @@
 import { useState, useMemo, useRef, useEffect } from "react";
 import { ArrowDown } from "./Icons";
-import {
-    Combobox,
-    ComboboxButton, // Added
-    ComboboxInput,
-    ComboboxOption,
-    ComboboxOptions,
-} from "@headlessui/react";
+import { Combobox, ComboboxInput, ComboboxOption } from "@headlessui/react";
 
 export default function SelectInput({
     id,
@@ -21,9 +15,28 @@ export default function SelectInput({
     required = false, // Added required prop
 }) {
     const [query, setQuery] = useState("");
+    const [open, setOpen] = useState(false);
     const inputRef = useRef(null); // Ref for the div containing ComboboxInput
     const comboboxInputRef = useRef(null); // Ref for the ComboboxInput itself
     const [inputWidth, setInputWidth] = useState("auto");
+    const containerRef = useRef(null);
+
+    // Cuando el usuario haga click fuera del contenedor, cierra el Combobox
+    useEffect(() => {
+        function handleClickOutside(event) {
+            // Si el contenedor existe y el click NO fue dentro de él...
+            if (
+                containerRef.current &&
+                !containerRef.current.contains(event.target)
+            ) {
+                setOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     useEffect(() => {
         if (!inputRef.current) return;
@@ -37,7 +50,7 @@ export default function SelectInput({
         return () => resizeObserver.disconnect();
     }, []);
 
-    // Effect to focus the ComboboxInput when isFocused is true
+    // Efecto para enfocar el input cuando isFocused es true
     useEffect(() => {
         if (isFocused && comboboxInputRef.current) {
             comboboxInputRef.current.focus();
@@ -56,79 +69,85 @@ export default function SelectInput({
     // Mostrar item.name en el input
     const displayValue = (item) => (item ? item.name : "");
 
-    // Clases base para los estilos
-    const baseOptionsContainerClasses =
-        "mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none empty:hidden";
-    const baseOptionClasses =
-        "relative cursor-default select-none py-2 px-4 text-gray-600 data-[focus]:bg-indigo-100";
-
     return (
-        <div className={`w-full ${className}`}>
+        <div className={`w-full ${className}`} ref={containerRef}>
             <Combobox
                 value={value}
                 onChange={onChange}
                 onClose={() => setQuery("")}
                 disabled={disabled}
             >
-                {/* Render prop to access the 'open' state of the Combobox */}
-                {({ open }) => (
-                    <>
-                        <div className="relative" ref={inputRef}>
-                            <ComboboxInput
-                                ref={comboboxInputRef} // Assign ref to ComboboxInput
-                                id={id}
-                                className={`w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm text-sm pr-10
-                                    ${
-                                        disabled
-                                            ? " opacity-70 cursor-not-allowed bg-gray-100 "
-                                            : ""
-                                    }
-                                    ${error ? " border-red-500" : ""}
-                                    ${value ? " bg-blue-50 " : " "}`}
-                                displayValue={displayValue} // Uses item.name for display
-                                onChange={(event) =>
-                                    setQuery(event.target.value)
-                                }
-                                placeholder={placeholder}
-                                autoComplete="off"
-                                // The required prop is removed from here
+                <>
+                    {/* Input y boton de despliegue del Combobox */}
+                    <div className="relative" ref={inputRef}>
+                        <ComboboxInput
+                            id={id}
+                            ref={comboboxInputRef}
+                            className={`w-full border-gray-300 focus:border-indigo-500 
+                                      focus:ring-indigo-500 rounded-md shadow-sm text-sm pr-10
+                                        ${
+                                            disabled
+                                                ? " opacity-70 cursor-not-allowed bg-gray-100 "
+                                                : ""
+                                        }
+                                        ${error ? " border-red-500" : ""}
+                                        ${value ? " bg-blue-50 " : " "}`}
+                            displayValue={displayValue} // Uses item.name for display
+                            onChange={(event) => setQuery(event.target.value)}
+                            placeholder={placeholder}
+                            autoComplete="off"
+                        />
+
+                        {/* Input oculto se reciba como prop required = true */}
+                        {required && (
+                            <input
+                                type="text"
+                                value={value ? value.id : ""}
+                                required
+                                className="sr-only top-8 right-24"
+                                aria-hidden="true"
+                                tabIndex={-1}
+                                onChange={() => {}}
+                                onFocus={(e) => e.target.blur()}
                             />
+                        )}
 
-                            {/* Visually hidden input to handle the 'required' attribute for form submission */}
-                            {required && (
-                                <input
-                                    type="text"
-                                    value={value ? value.id : ""} // Use a value that represents selection, like item.id
-                                    required
-                                    className="sr-only top-8 right-24" // Tailwind class for visually hidden
-                                    aria-hidden="true"
-                                    tabIndex={-1} // Make it non-focusable
-                                    onChange={() => {}} // React requires onChange for controlled inputs, even if it does nothing here
-                                    onFocus={(e) => e.target.blur()} // Prevent focus
-                                />
-                            )}
+                        {/* Boton para la apertura y cierre del combobox */}
+                        <button
+                            type="button"
+                            onClick={() => setOpen((prev) => !prev)}
+                            className="absolute inset-y-0 right-0 flex items-center 
+                                       rounded-r-md px-2 focus:outline-none"
+                        >
+                            <ArrowDown
+                                className={`text-gray-400 transition-transform duration-150 transform ${
+                                    open ? "rotate-180" : ""
+                                }`}
+                                aria-hidden="true"
+                                strokeWidth="1.8"
+                                size="20"
+                            />
+                        </button>
+                    </div>
 
-                            {/* Button to toggle the visibility of dropdown options */}
-                            <ComboboxButton className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                                <ArrowDown
-                                    className={`text-gray-400 transition-transform duration-150 transform ${
-                                        open ? "rotate-180" : ""
-                                    }`}
-                                    aria-hidden="true"
-                                    strokeWidth="1.8"
-                                    size="20"
-                                />
-                            </ComboboxButton>
-                        </div>
-
-                        <ComboboxOptions
+                    {/* Elementos */}
+                    {open && (
+                        <div
                             anchor="bottom"
                             transition
-                            className={`${baseOptionsContainerClasses} transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0:`}
+                            className={`mt-1 max-h-[188px] absolute overflow-auto rounded-md bg-white
+                                        py-1 text-sm shadow-lg ring-1 ring-black z-20
+                                        ring-opacity-5 focus:outline-none empty:hidden  
+                                        transition duration-100 ease-in-out
+                                        data-[leave]:data-[closed]:opacity-0:
+                                        ${open ? "flex flex-col" : "hidden"}`}
                             style={{ width: inputWidth }}
                         >
                             {filteredItems.length === 0 && query !== "" ? (
-                                <div className="relative cursor-default select-none py-2 px-4 text-sm text-gray-700">
+                                <div
+                                    className="relative cursor-default select-none 
+                                               py-2 px-4 text-sm text-gray-700"
+                                >
                                     Sin resultados.
                                 </div>
                             ) : (
@@ -136,7 +155,9 @@ export default function SelectInput({
                                     <ComboboxOption
                                         key={item.id} // Use item.id directly
                                         value={item}
-                                        className={`${baseOptionClasses} data-[selected]:font-semibold data-[selected]:bg-indigo-50`}
+                                        className={`relative cursor-default select-none py-2
+                                                    px-4 text-gray-600 data-[focus]:bg-indigo-100
+                                                    data-[selected]:font-semibold data-[selected]:bg-indigo-50`}
                                     >
                                         {/* Displays item.name in the option list */}
                                         <span className="block truncate">
@@ -145,9 +166,9 @@ export default function SelectInput({
                                     </ComboboxOption>
                                 ))
                             )}
-                        </ComboboxOptions>
-                    </>
-                )}
+                        </div>
+                    )}
+                </>
             </Combobox>
         </div>
     );
