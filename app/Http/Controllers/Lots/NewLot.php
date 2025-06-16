211b350c4340591738d769ebbbf5869baec62a73
franklinnot <franklinnot@outlook.com>
2025-06-16 05:58:10 -0500

    refactor: enhance create method in NewLot controller by adding transaction handling and logging operations

--- app/Http/Controllers/Lots/NewLot.php
+++ app/Http/Controllers/Lots/NewLot.php
@@ -7,9 +7,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Lot;
 use App\Enums\Status;
+use App\Enums\TypesOperation;
+use App\Models\Operation;
 use App\Models\Product;
 use Inertia\Inertia;
 use App\Utils\Report;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class NewLot extends Controller
 {
@@ -47,21 +51,37 @@ class NewLot extends Controller
             Report::error('Debe ingresar una fecha de vencimiento.', 'exp_date');
         }
 
-        $lot = Lot::create([
-            'code'           => $request->code,
-            'product_id'     => $request->product,
-            'stock'          => (int) $request->initial_stock,
-            'price'          => (float) $request->price,
-            'exp_alert'      => $request->exp_alert,
-            'exp_date'       => $exp_alert ? $exp_date : null,
-            'exp_status'     => $exp_alert ? ExpirationStatus::VALID->value : null,
-            'status' => Status::ENABLED->value,
-        ]);
+        $stock = (int) $request->initial_stock;
 
-        if (!$lot) {
-            Report::error('Error al registrar un nuevo lote.');
+        try {
+
+            DB::beginTransaction();
+
+            $lot = Lot::create([
+                'code'           => $request->code,
+                'product_id'     => $request->product,
+                'stock'          => $stock,
+                'price'          => (float) $request->price,
+                'exp_alert'      => $request->exp_alert,
+                'exp_date'       => $exp_alert ? $exp_date : null,
+                'exp_status'     => $exp_alert ? ExpirationStatus::VALID->value : null,
+                'status' => Status::ENABLED->value,
+            ]);
+
+            Operation::create([
+                'user_id' => Auth::user()->id,
+                'lot_id' => $lot->id,
+                'type' => TypesOperation::INPUT->value,
+                'quantity' => $stock,
+                'status' => Status::ENABLED->value,
+            ]);
+
+            DB::commit();
+
+            return Report::success(self::ROUTE, 'Lote registrado correctamente');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return Report::error('Error al registrar un nuevo lote.');
         }
-
-        return Report::success(self::ROUTE, 'Lote registrado correctamente');
     }
 }
